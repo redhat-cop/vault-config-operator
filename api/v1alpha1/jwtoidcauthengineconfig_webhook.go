2bc689261c208a2f4afcc761bc2a26c217c1872f
Erlis Balla <erlis.balla@lhind.dlh.de>
2022-10-12 18:57:14 +0200

    update(): update Controller for CRD JWTOIDCAuthEngineConfig & modify webhook.Validator & webook.Defaulter

--- api/v1alpha1/jwtoidcauthengineconfig_webhook.go
+++ api/v1alpha1/jwtoidcauthengineconfig_webhook.go
@@ -17,6 +17,8 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"errors"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -32,21 +34,16 @@ func (r *JWTOIDCAuthEngineConfig) SetupWebhookWithManager(mgr ctrl.Manager) erro
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
-//+kubebuilder:webhook:path=/mutate-redhatcop-redhat-io-v1alpha1-jwtoidcauthengineconfig,mutating=true,failurePolicy=fail,sideEffects=None,groups=redhatcop.redhat.io,resources=jwtoidcauthengineconfigs,verbs=create;update,versions=v1alpha1,name=mjwtoidcauthengineconfig.kb.io,admissionReviewVersions=v1
+//+kubebuilder:webhook:path=/mutate-redhatcop-redhat-io-v1alpha1-jwtoidcauthengineconfig,mutating=true,failurePolicy=fail,sideEffects=None,groups=redhatcop.redhat.io,resources=jwtoidcauthengineconfigs,verbs=create,versions=v1alpha1,name=mjwtoidcauthengineconfig.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Defaulter = &JWTOIDCAuthEngineConfig{}
 
 // Default implements webhook.Defaulter so a webhook will be registered for the type
 func (r *JWTOIDCAuthEngineConfig) Default() {
 	jwtoidcauthengineconfiglog.Info("default", "name", r.Name)
-
-	// TODO(user): fill in your defaulting logic.
 }
 
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-//+kubebuilder:webhook:path=/validate-redhatcop-redhat-io-v1alpha1-jwtoidcauthengineconfig,mutating=false,failurePolicy=fail,sideEffects=None,groups=redhatcop.redhat.io,resources=jwtoidcauthengineconfigs,verbs=create;update,versions=v1alpha1,name=vjwtoidcauthengineconfig.kb.io,admissionReviewVersions=v1
+//+kubebuilder:webhook:path=/validate-redhatcop-redhat-io-v1alpha1-jwtoidcauthengineconfig,mutating=false,failurePolicy=fail,sideEffects=None,groups=redhatcop.redhat.io,resources=jwtoidcauthengineconfigs,verbs=update,versions=v1alpha1,name=vjwtoidcauthengineconfig.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Validator = &JWTOIDCAuthEngineConfig{}
 
@@ -54,7 +51,6 @@ var _ webhook.Validator = &JWTOIDCAuthEngineConfig{}
 func (r *JWTOIDCAuthEngineConfig) ValidateCreate() error {
 	jwtoidcauthengineconfiglog.Info("validate create", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object creation.
 	return nil
 }
 
@@ -62,7 +58,10 @@ func (r *JWTOIDCAuthEngineConfig) ValidateCreate() error {
 func (r *JWTOIDCAuthEngineConfig) ValidateUpdate(old runtime.Object) error {
 	jwtoidcauthengineconfiglog.Info("validate update", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object update.
+	// the path cannot be updated
+	if r.Spec.Path != old.(*JWTOIDCAuthEngineConfig).Spec.Path {
+		return errors.New("spec.path cannot be updated")
+	}
 	return nil
 }
 
@@ -70,6 +69,5 @@ func (r *JWTOIDCAuthEngineConfig) ValidateUpdate(old runtime.Object) error {
 func (r *JWTOIDCAuthEngineConfig) ValidateDelete() error {
 	jwtoidcauthengineconfiglog.Info("validate delete", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object deletion.
 	return nil
 }
