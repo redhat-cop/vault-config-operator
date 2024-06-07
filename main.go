de6af1f8bcf9424a20167de391aeb8965ff7fb45
Erlis Balla <erlis.balla@lhind.dlh.de>
2024-06-07 10:45:14 +0200

    update(): update Controller Manager for AzureSecretEngineConfig CRD

--- main.go
+++ main.go
@@ -197,6 +197,11 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.AzureSecretEngineConfigReconciler{ReconcilerBase: vaultresourcecontroller.NewFromManager(mgr, "AzureSecretEngineConfig")}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "AzureSecretEngineConfig")
+		os.Exit(1)
+	}	
+
 	if err = (&controllers.QuaySecretEngineConfigReconciler{ReconcilerBase: vaultresourcecontroller.NewFromManager(mgr, "QuaySecretEngineConfig")}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "QuaySecretEngineConfig")
 		os.Exit(1)
@@ -310,7 +315,10 @@ func main() {
 			setupLog.Error(err, "unable to create webhook", "webhook", "GitHubSecretEngineRole")
 			os.Exit(1)
 		}
-
+		if err = (&redhatcopv1alpha1.AzureSecretEngineConfig{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "AzureSecretEngineConfig")
+			os.Exit(1)
+		}
 		if err = (&redhatcopv1alpha1.PKISecretEngineConfig{}).SetupWebhookWithManager(mgr); err != nil {
 			setupLog.Error(err, "unable to create webhook", "webhook", "PKISecretEngineConfig")
 			os.Exit(1)
@@ -368,17 +376,6 @@ func main() {
 		}
 	}
 
-	if err = (&controllers.AzureSecretEngineConfigReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "AzureSecretEngineConfig")
-		os.Exit(1)
-	}
-	if err = (&redhatcopv1alpha1.AzureSecretEngineConfig{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "AzureSecretEngineConfig")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
